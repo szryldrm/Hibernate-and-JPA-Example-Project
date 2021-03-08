51920cc6d9c56c98ab3c7141a2982bcbd927d3b1
Sezer YILDIRIM <sezer.yildirim@live.com>
2021-03-08 10:29:26 +0300

    Added JUnit Test And Remote Mysql DB Support

--- src/test/java/com/javacourse/project/hibernateAndJpa/HibernateAndJpaApplicationTests.java
+++ src/test/java/com/javacourse/project/hibernateAndJpa/HibernateAndJpaApplicationTests.java
@@ -1,13 +1,40 @@
 package com.javacourse.project.hibernateAndJpa;
 
+import com.javacourse.project.hibernateAndJpa.Business.ICityService;
+import com.javacourse.project.hibernateAndJpa.Entities.City;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class HibernateAndJpaApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@Autowired
+	private ICityService _cityService;
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Autowired
+	public HibernateAndJpaApplicationTests(ICityService cityService) {
+		_cityService = cityService;
 	}
 
+	@Test
+	void isCitiesFilled(){
+		int size = _cityService.getAll().size();
+		assertThat(size).isNotEqualTo(0);
+	}
+
+	@Test
+	@Transactional
+	void equalCityNameById(){
+		assertThat(_cityService.getById(5).getName()).isEqualTo("Amsterdam");
+	}
 }
